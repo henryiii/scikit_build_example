38750dc2e69376d9623249ed76ec19d8136c3ccc
Henry Schreiner <henryschreineriii@gmail.com>
2022-10-27 22:02:12 -0400

    feat: setuptools scikit-build-core example
    Signed-off-by: Henry Schreiner <henryschreineriii@gmail.com>

--- setup.py
+++ setup.py
@@ -1,27 +1,16 @@
 import sys
 
-try:
-    from skbuild import setup
-except ImportError:
-    print(
-        "Please update pip, you need pip 10 or greater,\n"
-        " or you need to install the PEP 518 requirements in pyproject.toml yourself",
-        file=sys.stderr,
-    )
-    raise
-
-from setuptools import find_packages
+from scikit_build_core.setuptools.extension import CMakeBuild, CMakeExtension
+from setuptools import find_packages, setup
 
 setup(
-    name="scikit_build_example",
+    name="cmake_example",
     version="0.0.1",
-    description="a minimal example package (with pybind11)",
-    author="Henry Schreiner",
-    license="MIT",
+    ext_modules=[CMakeExtension("scikit_build_example")],
+    zip_safe=False,
     packages=find_packages(where="src"),
     package_dir={"": "src"},
-    cmake_install_dir="src/scikit_build_example",
-    include_package_data=True,
-    extras_require={"test": ["pytest"]},
-    python_requires=">=3.6",
+    extras_require={"test": ["pytest>=6.0"]},
+    cmdclass={"build_ext": CMakeBuild},
+    python_requires=">=3.7",
 )
